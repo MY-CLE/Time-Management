c0b821961d8e03e015f10a23e549e75885e23343
Malte Budig <100218844+Doderich@users.noreply.github.com>
2022-10-26 15:15:14 +0200

    added qss sheet and calendar widget

--- src/UI/gui.py
+++ src/UI/gui.py
@@ -1,32 +1,50 @@
+from pathlib import Path
 import sys
 from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import(QApplication,QMainWindow, QHBoxLayout, QVBoxLayout, QPushButton)
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QHBoxLayout,
+                             QVBoxLayout, QPushButton, QWidget, QCalendarWidget)
+from PyQt6.QtGui import QPalette, QTextCharFormat 
+
+
+class Calender(QCalendarWidget):
+    def __init__(self):
+        super.__init__(self)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        
-        #Buttons
+        # Buttons
         testbutton = QPushButton()
-        testbutton.setba
+        testbutton.setObjectName("testbutton")
+        test2button = QPushButton()
         
+        #Calendar
         
+        self.calendar = Calender()
+
         # Layout
         mainQHlayout = QHBoxLayout()
         sidebarQVlayout = QVBoxLayout()
-        
+
         sidebarQVlayout.addWidget(testbutton)
+        sidebarQVlayout.addWidget(test2button)
         mainQHlayout.addLayout(sidebarQVlayout)
-        
-        central_widget = QtWidgets()
-        
-        central_widget.setLayout(mainQHlayout)
-        self.setCentralWidget(widget_widget)
-        
-        
-        
-app = QApplication(sys.argv)
+        mainQHlayout.addWidget(self.calendar)
 
-window = MainWindow()
-window.show()
+        centralWidget = QWidget()
 
-app.exec()
+        centralWidget.setLayout(mainQHlayout)
+        self.setCentralWidget(centralWidget)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    with open(Path("src/UI/styles.qss"), "r") as file:
+        app.setStyleSheet(file.read())
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
