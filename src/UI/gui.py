fc856db2e5d8728f5e45bee074f2ea0dbf8d46ee
Doderich <100218844+Doderich@users.noreply.github.com>
2022-11-09 14:16:24 +0100

    added Coments to pageSelect and loginWindow

--- src/UI/gui.py
+++ src/UI/gui.py
@@ -1,35 +1,27 @@
 import os
 import sys
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QHBoxLayout,
-                             QVBoxLayout, QPushButton, QWidget, QCalendarWidget,QLabel, QStackedWidget)
-from PyQt6.QtCore import QSize, QDir
-from Pages import main_window, login_window
+from PyQt6.QtWidgets import QApplication
+import pageSelect
 
 
 def main():
-    
+    #Starts Applijcation
     app = QApplication(sys.argv)
-    root = os.getcwd()    
+    
+    #Gets the root directory
+    root = os.getcwd()
     stylesheet = os.path.join(root, os.path.abspath("src/UI/styles.qss"))
     
+    #opens the Stylesheet to aply visual details
     with open(stylesheet, "r") as file:
         app.setStyleSheet(file.read())
     
     
-    widget = QStackedWidget()
-    loginWindow = login_window.LoginWindow()
-    mainWindow = main_window.MainWindow()
+    #creates a Pageselect objet which controles which window is shown
+    w =pageSelect.PageSelect()
     
-    widget.addWidget(mainWindow)
-    widget.addWidget(loginWindow)
-    widget.setCurrentWidget(loginWindow)
-    
-    widget.show()
-
     app.exec()
 
-    
 
 if __name__ == "__main__":
     main()
